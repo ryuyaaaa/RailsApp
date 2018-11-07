284a705dede1edf7e807b18a12cf2095f3ac4ff3
Ryuya Nishino <ryuyasamaaaa@gmail.com>
2018-11-07 17:19:13 +0900

    how to use controller and views

--- app/controllers/helo_controller.rb
+++ app/controllers/helo_controller.rb
@@ -1,5 +1,19 @@
 class HeloController < ApplicationController
 
   def index
+    if request.post? then
+      @title = 'Result'
+      if params['s1'] then
+        @msg = 'you selected: '
+        for val in params['s1']
+          @msg += val + ' '
+        end
+      else
+        @msg = 'not selected'
+      end
+    else
+      @title = 'Index'
+      @msg = 'select List'
+    end
   end
 end
