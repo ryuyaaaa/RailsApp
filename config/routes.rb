8bd1be35e505f51bd6f71a819142679fb195d252
Ryuya Nishino <ryuyasamaaaa@gmail.com>
2018-11-20 12:03:07 +0900

    start dengonban

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'dengonban/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
    get 'helo/index'
    get 'helo', to: 'helo#index'
