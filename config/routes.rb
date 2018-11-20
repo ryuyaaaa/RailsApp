e43a67a04b1bda72389158c7dd6bb573cb3e05bb
Ryuya Nishino <ryuyasamaaaa@gmail.com>
2018-11-20 17:02:18 +0900

    fix dengonban

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
+  get 'dengonban', to: 'dengonban#index'
+  post 'dengonban', to: 'dengonban#index'
   get 'dengonban/index'
+  post 'dengonban/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   get 'helo/index'
-   get 'helo', to: 'helo#index'
-   get 'helo/other'
-   post 'helo', to: 'helo#index'
-   post 'helo/index'
+  get 'helo/index'
+  get 'helo', to: 'helo#index'
+  get 'helo/other'
+  post 'helo', to: 'helo#index'
+  post 'helo/index'
 end
